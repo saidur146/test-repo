a5e5daa7f1eff44d0f2b238d73cd8557d901b458
saidur146 <saidur8922@gmail.com>
2024-09-01 05:44:46 +0000

    Hello.js

--- Hello.js
+++ Hello.js
@@ -1,4 +1,16 @@
-console.log("hi this ia saidur")
-for (let i = 0; i < 3; i++) {
-    console.log("keep going");
+const prompt = require("prompt-sync")();
+let a = prompt("what is your age?");
+//console.log(a);
+a = Number.parseInt(a);
+if(a<0){
+    console.log("This is an invalid age");
+}
+else if(a<9){
+    console.log("you are a kid and you cannot driving");
+}
+else if(a<18 && a>=9){
+    console.log("you are a kid and you can driving after 18");
+}
+else{
+    console.log("you can now drive as you are above 18");
 }
