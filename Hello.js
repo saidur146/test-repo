ebfe3fac5b7bebd624f3d7a45690f271a71adb49
saidur146 <saidur8922@gmail.com>
2024-09-01 05:56:27 +0000

    update

--- Hello.js
+++ Hello.js
@@ -1,16 +1,10 @@
+ //loop
+ let sum = 0
 const prompt = require("prompt-sync")();
-let a = prompt("what is your age?");
-//console.log(a);
-a = Number.parseInt(a);
-if(a<0){
-    console.log("This is an invalid age");
-}
-else if(a<9){
-    console.log("you are a kid and you cannot driving");
-}
-else if(a<18 && a>=9){
-    console.log("you are a kid and you can driving after 18");
-}
-else{
-    console.log("you can now drive as you are above 18");
-}
+let n = prompt("Enter the value of n = ")
+n = Number.parseInt(n)
+for (let i = 0; i < n; i++)  {
+    sum += (i+1)
+ }
+ console.log("sum of first " + n + " natural numbers is " + sum)
+
